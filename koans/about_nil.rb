a7664c072780649bd5185cd80f2d5cc29ebc7b55
Jusbin Haberwacky <joober1@seattlesmiths.com>
2017-04-29 13:48:39 -0700

    Working through exercises basics, nil

--- koans/about_nil.rb
+++ koans/about_nil.rb
@@ -8,31 +8,31 @@ class AboutNil < EdgeCase::Koan
   def test_you_dont_get_null_pointer_errors_when_calling_methods_on_nil
     #
     #  What is the Exception that is thrown when you call a method that
-    #  does not exist?  
+    #  does not exist?
     #
-    #  Hint:  launch irb and try the code in the block below.  
-    # 
-    #  Don't be confused by the code below yet.  It's using blocks 
+    #  Hint:  launch irb and try the code in the block below.
+    #
+    #  Don't be confused by the code below yet.  It's using blocks
     #  which are explained later on in about_blocks.rb.  For now,
     #  think about it like running nil.some_method_nil_doesnt_know_about
     #  in a sandbox and catching the error class into the exception
-    #  variable.  
+    #  variable.
     #
-    exception = assert_raise(___) do
+    exception = assert_raise(NoMethodError) do
       nil.some_method_nil_doesnt_know_about
     end
-    
-    # 
-    #  What is the error message itself? What substring or pattern could 
+
+    #
+    #  What is the error message itself? What substring or pattern could
     #  you test against in order to have a good idea what the string is?
-    #  
-    assert_match /__/, exception.message
+    #
+    assert_match /undefined method/, exception.message
   end
 
   def test_nil_has_a_few_methods_defined_on_it
-    assert_equal __, nil.nil?
-    assert_equal __, nil.to_s
-    assert_equal __, nil.inspect
+    assert_equal true, nil.nil?
+    assert_equal "", nil.to_s
+    assert_equal "nil", nil.inspect
 
     # THINK ABOUT IT:
     #
